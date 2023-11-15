e6680fef81bafdbe0302631e30c3e93eb267cb0e
putbullet <soulaianettabaas@gmail.com>
2023-11-15 02:48:37 -0800

    builtins

--- tests/builtin.c
+++ tests/builtin.c
@@ -6,18 +6,19 @@
  *
  * Return: 1 if the command is a builtin, 0 otherwise.
  */
+
 int cmp_builtins(char *comm)
 {
+	int i;
 	char *builtins_str[] = {"env", "exit", "NULL"};
 
-	for (size_t i = 0; builtins_str[i]; i++)
+	for (i = 0; builtins_str[i]; i++)
 	{
 		if (_strcmp(comm, builtins_str[i]) == 0)
-		{
 			return (1);
-		}
-		return (0);
 	}
+	
+	return (0);
 }
 
 /**
@@ -32,8 +33,7 @@ void handle_builtins(char **comm, char **argv, int *stats, int index)
 	(void)argv;
 
 	if (_strcmp(comm[0], "exit") == 0)
-		sh_exit(comm, stats, index);
-
+		sh_exit(comm, argv, stats, index);
 	else if (_strcmp(comm[0], "env") == 0)
 		sh_env(comm, stats);
 }
@@ -41,36 +41,34 @@ void handle_builtins(char **comm, char **argv, int *stats, int index)
 /**
  * sh_exit - Handle the 'exit' builtin command.
  * @comm: The command to handle.
+ * @argv: List of argv. Not examined.
  * @stats: Pointer to the shell status.
  * @index: The index of the command.
  */
-void sh_exit(char **comm, int *stats, int index)
+void sh_exit(char **comm, char **argv, int *stats, int index)
 {
-	int exit_val = (*stats);
 	char *idx, mssg[] = ": exit: Illegal number: ";
 
 	if (comm[1])
 	{
 		if (is_positive_nb(comm[1]))
-		{
-			exit_val = _atoi(comm[1]);
-		}
+			*stats = _atoi(comm[1]);
 		else
 		{
 			idx = _itoa(index);
 			write(STDERR_FILENO, argv[0], _strlen(argv[0]));
 			write(STDERR_FILENO, ": ", 2);
-			write(STDERR_FILENO, index, _strlen(index));
+			write(STDERR_FILENO, idx, _strlen(idx));
 			write(STDERR_FILENO, mssg, _strlen(mssg));
 			write(STDERR_FILENO, comm[1], _strlen(comm[1]));
 			write(STDERR_FILENO, "\n", 1);
 			free(idx);
-			free2DArray(comm);
+			free2DArray(comm, 0);
 			(*stats) = 2;
 			return;
 		}
 	}
-	free2DArray(comm);
+	free2DArray(comm, 0);
 	exit(*stats);
 }
 
@@ -83,12 +81,12 @@ void sh_env(char **comm, int *stats)
 {
 	int i;
 
-	for (i = 0; environ[i], _strlen(environ[i])); // <-- Check this line
+	for (i = 0; environ[i] && _strlen(environ[i]); i++)
 	{
-		write(STDOUT_FILENO, environ[i], _strlen(environ[i]))
+		write(STDOUT_FILENO, environ[i], _strlen(environ[i]));
 		write(STDOUT_FILENO, "\n", 1);
 	}
-	free2DArray(comm);
+	free2DArray(comm, 0);
 	(*stats) = 0;
 }
 
@@ -129,16 +127,13 @@ int _atoi(char *str)
 		i = 1;
 	}
 	else
-	{
 		i = 0;
-	}
 
-	for (; str[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		num *= 10;
 		num += (str[i] - '0');
 	}
-
 	return (sign * num);
 }
 
@@ -147,12 +142,11 @@ int _atoi(char *str)
  * @array: The 2D array.
  * @rows: The number of rows in the array.
  */
-void free2DArray(int **array, int rows)
+void free2DArray(char **array, int rows)
 {
-	for (int i = 0; i < rows; ++i)
-	{
+	int i;
+	for (i = 0; i < rows; ++i)
 		free(array[i]);
-	}
 
 	free(array);
 }
